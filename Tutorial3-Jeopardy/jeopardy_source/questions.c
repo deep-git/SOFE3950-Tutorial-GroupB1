9f8a33f27768b240bb544d36a9aa9594f52149be
deep-git <deepakthangella5@gmail.com>
2022-02-18 15:17:24 -0500

    Update questions.c, initialize game and already answered functions

--- Tutorial3-Jeopardy/jeopardy_source/questions.c
+++ Tutorial3-Jeopardy/jeopardy_source/questions.c
@@ -14,12 +14,35 @@
 void initialize_game(void)
 {
     // initialize each question struct and assign it to the questions array
+    for (int i = 0; i < NUM_QUESTIONS; i++) {
+      strcpy(questions[i].category, categories[i % NUM_CATEGORIES]);
+      questions.answered = false;
+    }
+
+    strcpy(questions[0].question, "what is 1+1");
+    strcpy(questions[1].question, "what is 2+2");
+    strcpy(questions[2].question, "what is 3+3");
+    strcpy(questions[3].question, "what is 4+4");
+    strcpy(questions[4].question, "what is 5+5");
+    strcpy(questions[5].question, "what is 6+6");
+    strcpy(questions[6].question, "what is 7+7");
+    strcpy(questions[7].question, "what is 8+8");
+    strcpy(questions[8].question, "what is 9+9");
+    strcpy(questions[9].question, "what is 10+10");
+    strcpy(questions[10].question, "what is 11+11");
+    strcpy(questions[11].question, "what is 12+12");
+
+
 }
 
 // Displays each of the remaining categories and question dollar values that have not been answered
 void display_categories(void)
 {
     // print categories and dollar values for each unanswered question in questions array
+    for (int i = 0; i < NUM_QUESTIONS; i++) {
+      if (question[i].answered ==  false) {
+      }
+    }
 }
 
 // Displays the question for the category and dollar value
@@ -32,6 +55,7 @@ void display_question(char *category, int value)
 bool valid_answer(char *category, int value, char *answer)
 {
     // Look into string comparison functions
+
     return false;
 }
 
@@ -39,5 +63,14 @@ bool valid_answer(char *category, int value, char *answer)
 bool already_answered(char *category, int value)
 {
     // lookup the question and see if it's already been marked as answered
+    for (int i = 0; i < NUM_QUESTIONS; i++) {
+
+      // Checks to see if the questions value and the category match according to a previous entry, if it does then returns true as the question has already been answered
+      if (questions[i].value == value && strcmp(questions[i].category, category) == 0) {
+        return true;
+      }
+    }
+
+    // Returns false if the question has not yet been answered
     return false;
 }
