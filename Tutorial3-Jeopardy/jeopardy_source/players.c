de3b922af86567a119b5924cecb46ce504c77f75
deep-git <deepakthangella5@gmail.com>
2022-02-18 14:32:58 -0500

    Update players.c with complete functions

--- Tutorial3-Jeopardy/jeopardy_source/players.c
+++ Tutorial3-Jeopardy/jeopardy_source/players.c
@@ -13,12 +13,32 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+  for (int i = 0; i < num_players; i++) {
+
+    // Checks to see if the players name matches the given player name character by character
+    name_compare = strcmp(players[i].name, name);
+
+    // If the names are equal, then returns true
+    if (name_compare == 0) {
+      return true;
+    }
+  }
+
     return false;
 }
 
-// Go through the list of players and update the score for the 
+// Go through the list of players and update the score for the
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+    for (int i = 0; i < num_players; i++) {
+
+      // Checks to see if the players name matches the given player name character by character
+      name_compare = strcmp(players[i].name, name);
+
+      // If the names are equal, then update the score of the player
+      if (name_compare == 0) {
+        players[i].score = players[i].score + score;
+      }
+    }
 }
