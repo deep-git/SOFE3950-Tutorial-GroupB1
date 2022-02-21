ba48bb1609294dec13eac27be55fe869071f5e96
deep-git <deepakthangella5@gmail.com>
2022-02-20 23:24:55 -0500

    Revert "Update display_question function"
    This reverts commit c0676eb9e2f87362ed0dd050d6d5d669771863e2.

--- Tutorial3-Jeopardy/jeopardy_source/questions.c
+++ Tutorial3-Jeopardy/jeopardy_source/questions.c
@@ -124,8 +124,11 @@ void display_categories(void)
 // Displays the question for the category and dollar value
 void display_question(char *category, int value)
 {
-  int questionNum = question_num(category, value);
-  printf("%s", questions[questionNum].question);
+    for (int i=0; i < NUM_QUESTIONS; i++){
+      if (questions[i].value == value && strcmp(questions[i].category, category) == 0){
+        printf("%s",questions[i].question);
+      }
+    }
 }
 
 // Returns true if the answer is correct for the question for that category and dollar value
@@ -134,15 +137,6 @@ bool valid_answer(char *category, int value, char *answer)
     // Look into string comparison functions
     int questNum = question_num(category, value);
     return (strcasecmp(answer, questions[questNum].answer) == 0);
-}
-
-// Determines which question number the player is on
-int question_num(char *category, int value) {
-  for (int i = 0; i < NUM_QUESTIONS; i++) {
-    if (questions[i].value == value && strcmp(questions[i].category, category) == 0) {
-      return (i);
-    }
-  }
 }
 
 // Returns true if the question has already been answered
